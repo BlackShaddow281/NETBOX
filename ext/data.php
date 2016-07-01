deb9cf7fb948a0930d0e8c7522a5063666a68c18
BlackShaddow281 <m.rosenmueller1@gmm.muenchen.musin.de>
2016-07-01 12:26:15 +0200

    Revert "Maximilian Rosenmüller"
    This reverts commit 55182a9ed2570aeb294479c0e4f94005b82bddb0.

--- ext/data.php
+++
@@ -1,1 +0,0 @@
-<?php    /**    * Mini-Muenchen Bank    * @author Tobias Schwarz    * @version 1.4    *     * In this file the MySQL Data is saved    */    $mysqlhost='127.0.0.1';    $mysqluser='root';    $mysqlpass='';    $mysqldb='server';    ?>
